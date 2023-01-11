ba6781e8b7c5bd6e42e6b4f0928553f9aa6280df
Rahma Henchi <rahmahenchi@gmail.com>
2023-01-11 21:31:21 +0100

    installation du bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Contact from './Pages/Contact';
+import Home from './Pages/Home'
+import Projects from './Pages/Projects';
+import Skills from './Pages/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Route path='/' component={Home} exact />
+          <Route path='/skills' component={Skills} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/contact' component={Contact} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item nav-link">
+              {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
+              <NavLink to='/' exact>Home</NavLink>
+            </li>
+            <li class="nav-item nav-link">
+              <NavLink to='/skills'>Skills</NavLink>
+            </li>
+            <li class="nav-item nav-link">
+              <NavLink to='/projects' >Projects</NavLink>
+            </li>
+            <li class="nav-item nav-link">
+              <NavLink to='/contact' >Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
