9198b680245436b996fb950e856a1c93702a61f3
Rahma Henchi <rahmahenchi@gmail.com>
2023-01-15 18:13:46 +0100

    import notfound and react router switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { BrowserRouter, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import Contact from './Pages/Contact';
-import Home from './Pages/Home'
-import Projects from './Pages/Projects';
-import Skills from './Pages/Skills';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Contact from './pages/Contact';
+import Home from './pages/Home'
+import NotFound from './pages/NotFound';
+import Projects from './pages/Projects';
+import SkillDetails from './pages/SkillDetails';
+import Skills from './pages/Skills';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-        <div className='container'>
-          <Route path='/' component={Home} exact />
-          <Route path='/Skills' component={Skills} />
-          <Route path='/Projects' component={Projects} />
-          <Route path='/Contact' component={Contact} />
+        <div className='container mt-5'>
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/skills/:identifier' component={SkillDetails} />
+            <Route path='/skills' component={Skills} />
+            <Route path='/projects' component={Projects} />
+            <Route path='/contact' component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </BrowserRouter>
     </div>
@@ -22,4 +28,3 @@ function App() {
 }
 
 export default App;
-
