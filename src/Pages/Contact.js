c2e4b43ecec08b46b1f9867865a3c862f235913b
Rahma Henchi <rahmahenchi@gmail.com>
2023-01-15 17:16:08 +0100

    added informations from the contact.js page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+const contactInfo = {
+  Phone: '+216 50 657 576',
+  Email: 'rahmahenchi@gmail.com',
+  Address: 'Route aéroport km 6, Av. la liberté 3078 Sfax Tunisie'
+}
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='contact-item'>
+        <i class="bi bi-telephone-fill"></i>
+        <p>{contactInfo.Phone}</p>
+      </div>
+      <div className='contact-item'>
+        <i class="bi bi-envelope"></i>
+        <p>{contactInfo.Email}</p>
+      </div>
+      <div className='contact-item'>
+        <i class="bi bi-geo-alt-fill"></i>
+        <p>{contactInfo.Address}</p>
+      </div>
+    </div>
   )
 }
 
