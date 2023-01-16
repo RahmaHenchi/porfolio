f16bc9a763c5a9bcd2281095eb336b2930fe1fff
Rahma Henchi <rahmahenchi@gmail.com>
2023-01-16 23:04:49 +0100

    replace my photo profile

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ import webImage from '../Images/photo-2.png'
 function Home() {
   return (
     <div className="alert alert-info" role="alert">
-      <img src="/photo-1.jpg" alt="Profile" style={{ borderRadius: '50%' }} />
+      <img src="/cc 038.jpg" alt="Profile" style={{ borderRadius: '50%', width: '250px', height: '250px' }} />
       <p>Hello. My name is Rahma. I am a Fullsatack Web Developer</p>
       <img src={webImage} alt='Web' />
     </div>
