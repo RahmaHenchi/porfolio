91f5e7d9276285a7212c06169db48c03638fcc8c
Rahma Henchi <rahmahenchi@gmail.com>
2023-05-09 01:59:30 +0200

    Add new projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -3,28 +3,44 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div className='col-12'>
-      <div class="card" style={{ width:300, marginBottom:20 }}>
+    <div class='col-12'>
+
+      <div class="card" style={{ width: 300, marginBottom: 20 }}>
         <img src='/Image1.png' class="card-img-top" alt="Counter" />
         <div class="card-body">
           <h5 class="card-title">Advanced Counter</h5>
         </div>
       </div>
-      <div class="card" style={{ width:300, marginBottom:20 }}>
+
+      <div class="card" style={{ width: 300, marginBottom: 20 }}>
         <img src='/Image2.png' class="card-img-top" alt="ToDom" />
         <div class="card-body">
           <h5 class="card-title">ToDom</h5>
         </div>
-        <div class="card" style={{ width:300, marginBottom:20 }}>
-          <img src='/Image3.png' class="card-img-top" alt="Business" style={{}} />
-          <div class="card-body">
-            <h5 class="card-title">Business Land</h5>
-          </div>
+      </div>
+
+      <div class="card" style={{ width: 300, marginBottom: 20 }}>
+        <img src='/Image3.png' class="card-img-top" alt="Business" />
+        <div class="card-body">
+          <h5 class="card-title">Business Land</h5>
         </div>
       </div>
-    </div>
 
+      <div class="card" style={{ width: 300, marginBottom: 20 }}>
+        <img src='/mercado.png' class="card-img-top" alt="Mercado" />
+        <div class="card-body">
+          <h5 class="card-title">Mercado</h5>
+        </div>
+      </div>
+
+      <div class="card" style={{ width: 300, marginBottom: 20 }}>
+        <img src='/agenda.png' class="card-img-top" alt="Agenda" />
+        <div class="card-body">
+          <h5 class="card-title">Agenda</h5>
+        </div>
+      </div>
+
+    </div>
   )
 }
-
 export default Projects
