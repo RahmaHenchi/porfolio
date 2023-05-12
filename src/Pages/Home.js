1bf986d429d38520451f6d6ff64f553f04418075
Rahma Henchi <rahmahenchi@gmail.com>
2023-05-13 00:37:02 +0200

    Modification

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import webImage from '../Images/photo-2.png'
 
 function Home() {
   return (
     <div className="alert alert-info" role="alert">
       <img src="/cc 038.jpg" alt="Profile" style={{ borderRadius: '50%', width: '250px', height: '250px' }} />
       <p>Hello. My name is Rahma. I am a Fullstack Web Developer</p>
-      <img src={webImage} alt='Web' />
+      <img src="/photo-2.png" alt='Web' />
     </div>
   )
 }
