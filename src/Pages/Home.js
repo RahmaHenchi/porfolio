d10621a34a3837ac02e30f055a226a46511a6e69
Rahma Henchi <rahmahenchi@gmail.com>
2023-05-12 23:44:29 +0200

    Correction

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="alert alert-info" role="alert">
       <img src="/cc 038.jpg" alt="Profile" style={{ borderRadius: '50%', width: '250px', height: '250px' }} />
-      <p>Hello. My name is Rahma. I am a Fullsatack Web Developer</p>
+      <p>Hello. My name is Rahma. I am a Fullstack Web Developer</p>
       <img src={webImage} alt='Web' />
     </div>
   )
